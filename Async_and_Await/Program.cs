835d5e60038a66e077924b65dd21d6dfe5b0aed0
White_Owl <desiderio.giovanni92@gmail.com>
2020-03-28 18:51:42 +0100

    Aggiunta parte iniziale del progetto su Async and Await

--- Async_and_Await/Program.cs
+++ Async_and_Await/Program.cs
@@ -12,6 +12,30 @@ namespace Async_and_Await
         {
             Console.WriteLine("Hello, I am a newbie but i hope one day I can become a Legend");
 
+            Console.WriteLine("First we are inside the main method...");
+            RunProcess(1, 1_000_000);   //Ho dichiarato il primo 
+            RunProcess(2, 1_000_000);   //e il secondo processo
+
+            Console.ReadLine();
+
+            Console.WriteLine("Test");
+            Console.WriteLine("Test");
+            Console.WriteLine("Test");
+
+        }
+
+        private static void RunProcess(int processNumber, int processLoopCondition)
+        {
+            Console.WriteLine("\nControl is with process " + processNumber);
+            Console.WriteLine($"Process ({processNumber}) has started.");
+            Console.WriteLine($"Process ({processNumber}) is running...");
+            for (int i = 0; i < processLoopCondition; i++)
+            {
+            }
+            Console.WriteLine($"Process ({processNumber}) is complete");
+            Console.WriteLine("Control is back to main method");
         }
     }
+
 }
+
